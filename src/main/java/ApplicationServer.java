a011dd83c2e358b823a8c4fc9382cb9167058608
ubektas <ubektas@innova.com.tr>
2022-09-30 14:30:06 +0300

    GRPC

--- src/main/java/ApplicationServer.java
+++ src/main/java/ApplicationServer.java
@@ -1,16 +1,34 @@
 import io.grpc.Server;
 import io.grpc.ServerBuilder;
+import io.grpc.netty.shaded.io.grpc.netty.NettyServerBuilder;
 import server.OrderServiceImpl;
 
 import java.io.IOException;
+import java.util.Objects;
 
 /**
  * @author ubektas
  */
+
 public class ApplicationServer {
-    public static void main(String[] args) throws IOException, InterruptedException {
-        Server server = ServerBuilder.forPort(8087).addService(new OrderServiceImpl()).build();
+    private Server server;
+    private int port = 8087;
+
+    private void start() throws Exception {
+        server = NettyServerBuilder.forPort(port).addService(new OrderServiceImpl()).build();
         server.start();
         server.awaitTermination();
+    }
+
+    private void stop() {
+        if (!Objects.isNull(server)) {
+            server.shutdown();
+        }
+    }
+
+
+    public static void main(String[] args) throws Exception {
+        final ApplicationServer applicationServer = new ApplicationServer();
+        applicationServer.start();
     }
 }
